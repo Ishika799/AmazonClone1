6177872a9701d8f37e062cb6c999b5f1a6143262
Ishika <ishikakumari392002@gmail.com>
2023-04-02 13:30:53 +0530

    initial commit

---
+++ db/connection.js
@@ -0,0 +1,4 @@
+const mongoose =require('mongoose');
+const DB=process.env.DATABASE;
+mongoose.set('strictQuery', false)
+mongoose.connect(DB).then(()=>console.log("database connected")).catch((error)=>console.log('error'+error.message))
